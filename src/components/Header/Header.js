c29517e591861ded627a9fa4d6b018a7458a6963
SamJHudson01 <samjhudson@gmail.com>
2023-08-10 17:51:38 +0100

    layout updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,8 +14,9 @@ const Header = ({handleSignInClick, handleSignOutClick, handleSearch}) => {
         <div className="header__content">
             <h1 className="header__title">What's going on in the world?</h1>
             <p className="header__description">Find the latest news on any topic and save them in your personal account.</p>
+            <SearchForm handleSearch={handleSearch}/>
         </div>
-        <SearchForm handleSearch={handleSearch}/>
+        
     </header>
   )
 }
